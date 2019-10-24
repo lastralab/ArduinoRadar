35196974ce416b6c3bb95976cc5d361a1a1bb9b3
yashchavan1997 <43619763+yashchavan1997@users.noreply.github.com>
2019-10-24 17:10:02 +0530

    Added support for continuous rotation servo motors

--- Radar.ino
+++ Radar.ino
@@ -7,6 +7,9 @@
 const int trigPin = 7;
 const int echoPin = 8;
 
+int servoType = 180   //360 for continuous servo
+int servoSpeed = 130  //Servo speed in RPM
+
 long duration;
 int distance;
 Servo myServo; 
@@ -16,32 +19,52 @@ void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
   Serial.begin(9600);
-  myServo.attach(9); 
+  myServo.attach(9);
+  
+  myServo.write(90);    //stop
 }
 
 void loop() {
   
   for(int i=15;i<=165;i++){  
-  myServo.write(i);
-  delay(30);
-  distance = calculateDistance();
-  
-  Serial.print(i); 
-  Serial.print(","); 
-  Serial.print(distance); 
-  Serial.print("."); 
+    if(servoType == 180)
+    {
+      myServo.write(i);
+    }
+    else if(servoType == 360)
+    {
+      myServo.write(180);   //Rotate CW
+      delay(((1/360)/servoSpeed)*60000);
+      myServo.write(90);    //stop
+    }
+    delay(30);
+    distance = calculateDistance();
+
+    Serial.print(i); 
+    Serial.print(","); 
+    Serial.print(distance); 
+    Serial.print("."); 
   }
   
   for(int i=165;i>15;i--){  
-  myServo.write(i);
-  delay(30);
-  
-  distance = calculateDistance();
-  
-  Serial.print(i);
-  Serial.print(",");
-  Serial.print(distance);
-  Serial.print(".");
+    if(servoType == 180)
+    {
+      myServo.write(i);
+    }
+    else if(servoType == 360)
+    {
+      myServo.write(0);   //Rotate CCW
+      delay(((1/360)/servoSpeed)*60000);
+      myServo.write(90);    //stop
+    }
+    delay(30);
+
+    distance = calculateDistance();
+
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
   }
 }
 
